c7c16d18bd6760c04898e1453f6fcb3fb4c7433e
MartinTropse <martin.tropse.andersson@gmail.com>
2020-04-13 21:51:32 +0100

    ResampleScript.py

--- ResampleScript.py
+++ ResampleScript.py
@@ -4,6 +4,8 @@ Created on Sun Mar 15 10:14:34 2020
 @author: MartinTropse
 """
 
+#Script to resample all tif-files to 10*10 raster. 
+
 import os
 import time
 import re
@@ -16,4 +18,5 @@ for dirc, subdir, files in os.walk("D:/GIS/Data/Raster/Symphony/National_eco_E_2
         if a:
             print(f"Woaw it looks like a file {file}")
             processing.run("grass7:r.resample", {'input':f'D:/GIS/Data/Raster/Symphony/National_eco_E_2018/{file}','output':f'D:/GIS/Data/Raster/Symphony/National_eco_E_2018/10m_{file}','GRASS_REGION_PARAMETER':None,'GRASS_REGION_CELLSIZE_PARAMETER':10,'GRASS_RASTER_FORMAT_OPT':'','GRASS_RASTER_FORMAT_META':''})
-print("I be damn, we made it!")    
+print("I be damn, we made it!")
+print("It is true!")    
